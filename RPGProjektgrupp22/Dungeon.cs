2cca69f9f2e5595f2198edaa4d75bf202c2e8d93
Benjamin Mellander <147602603+bmellander@users.noreply.github.com>
2023-11-21 16:17:33 +0100

    Lagt in Dungeons

---
+++ RPGProjektgrupp22/Dungeon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGProjektgrupp22
+{
+    internal class Dungeon
+    {
+        private List<string> dungeonNameList = new List<string>() { "Ice Dungeon", "Fire Dungeon", "Noob Dungeon", "Water dungeon"};
+        private string dungeonName;
+        private static Random generator = new Random();
+        public Dungeon()
+        {
+            dungeonName = dungeonNameList[generator.Next(1, dungeonNameList.Count())];
+        }   
+    }
+}
--- RPGProjektgrupp22/Program.cs
+++ RPGProjektgrupp22/Program.cs
@@ -5,6 +5,11 @@
         static void Main(string[] args)
         {
             new Story();
+            Queue<Dungeon> r1Queue = new Queue<Dungeon>();
+            for (int i = 0; i < 4 + 1; i++)
+            {
+                r1Queue.Enqueue(new Dungeon());
+            }
         }
     }
 }
