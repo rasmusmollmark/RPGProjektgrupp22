e8c28f1c390a7f2cf30ae8fd734678f6581190d5
Benjamin Mellander <BMellander@outlook.com>
2023-11-23 11:29:41 +0100

    Fixade loopen med dungeons.

--- RPGProjektgrupp22/Dungeon.cs
+++ RPGProjektgrupp22/Dungeon.cs
@@ -12,24 +12,39 @@ namespace RPGProjektgrupp22
         private Random rnd = new Random();
         private int dungeonsCleared = 0;
         private int dungeonIndex;
+        private bool explored = false;
+        public bool Explored 
+        {
+            get { return explored; }
+        }
         public Dungeon(int i)
         {
             dungeonIndex = i;
             Console.WriteLine("A dungeon named" + dungeonNameList[i] + " has been created");
         } 
-        public void Explore(Player player)
+        public bool Explore (Player player)
         {
             int randomOutcome = rnd.Next(1, 10);
-
-            if (randomOutcome > 6) 
+            if (explored)
+            {
+                Console.WriteLine("This dungeon has already been explored");
+                return false;
+            }
+            if (randomOutcome < 6) 
             {
                 Console.WriteLine("You encounter an enemy");
+                EncounterEnemy();
+                //Console.WriteLine("You are in the" + dungeonNameList[dungeonIndex]);
             }
 
             else
             {
                 Console.WriteLine("You found a chest!");
+                FindChest();
+                //Console.WriteLine("You are in the" + dungeonNameList[dungeonIndex]);
             }
+            explored = true;
+            return true;
 
 
         }
@@ -42,12 +57,18 @@ namespace RPGProjektgrupp22
         private void EncounterEnemy()
         {
             Console.WriteLine("You have encountered an enemy!");
-            
+            Console.WriteLine("You fought fiercely and succeded!");
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+
+
         }
 
         private void FindChest()
         {
             Console.WriteLine("You found a chest with items!");
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
 
 
 
